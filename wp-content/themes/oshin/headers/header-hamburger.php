67bfab591f2d8f2059f04b49620fcfe364d99a86
Maria Zhekova <mzhekova97@gmail.com>
2023-02-16 12:15:52 +0100

    edit Robots section

--- wp-content/themes/oshin/headers/header-hamburger.php
+++
@@ -1,5 +0,0 @@
-<span class="be-mobile-menu-icon ">   
-        <span class="hamburger-line-1"></span>
-        <span class="hamburger-line-2"></span>
-        <span class="hamburger-line-3"></span>
-</span>
